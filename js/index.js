cfd074bcabb0f01c35574ecea943b90eb5623a22
golap-raj <golapraj47@gmail.com>
2025-02-27 23:25:02 +0600

    resolve

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// function btnStyel (){
+//     document.body.style.backgroundColor = "blue"
+// }
+// function btnStyels (){
+//     document.body.style.backgroundColor = "red"
+// }
+
+const btnBlue = document.getElementById("btn")
+btnBlue.onclick = function blue (){
+    document.body.style.backgroundColor = "yellow"
+
+}
