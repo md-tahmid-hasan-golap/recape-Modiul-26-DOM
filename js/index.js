1b89fcc7fa6488dd41daf526b437881af72afff6
golap-raj <golapraj47@gmail.com>
2025-02-27 23:35:31 +0600

    button

--- js/index.js
+++ js/index.js
@@ -5,8 +5,12 @@
 //     document.body.style.backgroundColor = "red"
 // }
 
-const btnBlue = document.getElementById("btn")
-btnBlue.onclick = function blue (){
-    document.body.style.backgroundColor = "yellow"
+// const btnBlue = document.getElementById("btn")
+// btnBlue.onclick = function blue (){
+//     document.body.style.backgroundColor = "yellow"
 
-}
+// }
+
+document.getElementById("btn_onclick").addEventListener("click", function () {
+    document.body.style.backgroundColor = "red"
+})
