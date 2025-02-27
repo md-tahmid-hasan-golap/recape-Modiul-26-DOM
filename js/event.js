0b3460d78f6fbf3a535d9eb66d4641c5ecc5b76b
golap-raj <golapraj47@gmail.com>
2025-02-28 00:19:13 +0600

    resolve

---
+++ js/event.js
@@ -0,0 +1,5 @@
+document.getElementById("btn_your_name").addEventListener("click", function(){
+
+     const heading = document.getElementById("heading_title");
+     heading.innerText = "My Name Is Button"
+})
--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-// function btnStyel (){
-//     document.body.style.backgroundColor = "blue"
-// }
-// function btnStyels (){
+// // function btnStyel (){
+// //     document.body.style.backgroundColor = "blue"
+// // }
+// // function btnStyels (){
+// //     document.body.style.backgroundColor = "red"
+// // }
+
+// // const btnBlue = document.getElementById("btn")
+// // btnBlue.onclick = function blue (){
+// //     document.body.style.backgroundColor = "yellow"
+
+// // }
+
+// document.getElementById("btn_onclick").addEventListener("click", function () {
 //     document.body.style.backgroundColor = "red"
-// }
+// })
 
-// const btnBlue = document.getElementById("btn")
-// btnBlue.onclick = function blue (){
-//     document.body.style.backgroundColor = "yellow"
-
-// }
-
-document.getElementById("btn_onclick").addEventListener("click", function () {
-    document.body.style.backgroundColor = "red"
-})
+// document.getElementById("btn_whats_your_name").addEventListener("click", function(){
+//     const changeItem = document.getElementById("My_name_btn");
+//     changeItem.innerText = "My Name Is Button"
+// })
