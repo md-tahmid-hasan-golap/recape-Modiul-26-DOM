4ae97bfad0dba04196f6136da6508c0409c09dd2
golap-raj <golapraj47@gmail.com>
2025-02-28 00:37:41 +0600

    resolve

--- js/event.js
+++ js/event.js
@@ -1,5 +1,16 @@
-document.getElementById("btn_your_name").addEventListener("click", function(){
+// document.getElementById("btn_your_name").addEventListener("click", function(){
 
-     const heading = document.getElementById("heading_title");
-     heading.innerText = "My Name Is Button"
+//      const heading = document.getElementById("heading_title");
+//      heading.innerText = "My Name Is Button"
+// })
+
+document.getElementById("click_btn").addEventListener("click", function(){
+
+      const inputFild = document.getElementById("input_fild");
+      const name = inputFild.value;
+      const setElement = document.getElementById("set_element");
+
+      setElement.innerHTML = name
+      
+
 })
